4d4cf4d86f39ef6ee52159c90b544974788b9474
Nikos Karagiannakis <nikoskaragiannakis@gmail.com>
2018-04-02 19:08:13 +0100

    Code executed in separate cells. No need for a main() function

--- d_jupyter_kernel/kernel.py
+++ d_jupyter_kernel/kernel.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 import subprocess
 import tempfile
 
@@ -15,6 +16,22 @@ class DKernel(Kernel):
     language_info = {'name': 'd',
                      'mimetype': 'text/plain',
                      'file_extension': '.d'}
+
+    def __init__(self, **kwargs):
+        self.temp_dir = tempfile.mkdtemp(dir=os.getcwd())
+
+        self.import_statements = set()
+        self.funcs = set()
+
+        self.func_def_pattern = re.compile(r"^\s*(\w+\s+)?\s*\w+\s+(\w+)\s*\([^)]*\)\s*\{.*?",
+                                           re.MULTILINE)
+        self.import_pattern = re.compile(r"^import\s+[\w+.,: ]+;")
+
+        super().__init__(**kwargs)
+
+    def __exit__(self):
+        if os.path.exists(self.temp_dir):
+            shutil.rmtree(self.temp_dir)
 
     @property
     def implementation_version(self):
@@ -36,35 +53,57 @@ class DKernel(Kernel):
                    allow_stdin=True):
         self._allow_stdin = allow_stdin
         if not silent:
-            with tempfile.TemporaryDirectory() as temp_dir:
-                with tempfile.NamedTemporaryFile(suffix='.d', mode='w', dir=temp_dir) as d_file:
-                    d_file.write(code)
-                    d_file.flush()
-                    proc = subprocess.Popen(['dmd', d_file.name, '-op'],
-                                            bufsize=0,
-                                            cwd=temp_dir,
-                                            stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE)
-                    stdout, stderr = proc.communicate()
-                    if stderr:
-                        rows = stderr.decode('utf-8').split('\n')
-                        rows = [''.join(row.replace(d_file.name, 'line')) for row in rows]
-                        stderr = '\n'.join(rows)
-                        self.send_response(self.iopub_socket, 'stream',
-                                           {'name': 'stdout', 'text': stderr})
-                        return {'status': 'ok', 'execution_count': self.execution_count,
-                                'payload': [], 'user_expressions': {}}
 
-                    exec_response = subprocess.Popen('{}'.format(d_file.name.split('.')[0]),
-                                                     stdout=subprocess.PIPE,
-                                                     stdin=subprocess.PIPE,
-                                                     stderr=subprocess.PIPE)
-                    res = exec_response.communicate()
-                    self.send_response(self.iopub_socket, 'stream',
-                                       {'name': 'stdout', 'text': res[0].decode('utf-8')})
+            if re.match(self.func_def_pattern, code):
+                self.funcs.add(code)
+                return {'status': 'ok', 'execution_count': self.execution_count,
+                        'payload': [], 'user_expressions': {}}
+            elif re.match(self.import_pattern, code):
+                self.import_statements.add(code)
+                return {'status': 'ok', 'execution_count': self.execution_count,
+                        'payload': [], 'user_expressions': {}}
 
-                    return {'status': 'ok', 'execution_count': self.execution_count, 'payload': [],
-                            'user_expressions': {}}
+            d_file = tempfile.NamedTemporaryFile(prefix='d_main_',
+                                                 suffix='.d',
+                                                 mode='w',
+                                                 dir=self.temp_dir)
+
+            import_str = '\n'.join(self.import_statements) if self.import_statements else ''
+            funcs_str = '\n'.join(self.funcs) if self.funcs else ''
+
+            main_str = "void main() {{{}}}".format(code)
+
+            d_file.write('{}\n\n{}\n\n{}'.format(import_str, funcs_str, main_str))
+            d_file.flush()
+
+            proc = subprocess.Popen(['dmd',
+                                     d_file.name,
+                                     '-op'],
+                                    bufsize=0,
+                                    cwd=self.temp_dir,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+            stdout, stderr = proc.communicate()
+            if stderr:
+                rows = stderr.decode('utf-8').split('\n')
+                rows = [''.join(row.replace(d_file.name, 'line')) for row in rows]
+                stderr = '\n'.join(rows)
+                self.send_response(self.iopub_socket, 'stream',
+                                   {'name': 'stdout', 'text': stderr})
+                os.remove(d_file.name)
+                return {'status': 'ok', 'execution_count': self.execution_count,
+                        'payload': [], 'user_expressions': {}}
+
+            exec_response = subprocess.Popen('{}'.format(d_file.name.split('.')[0]),
+                                             stdout=subprocess.PIPE,
+                                             stdin=subprocess.PIPE,
+                                             stderr=subprocess.PIPE)
+            res = exec_response.communicate()
+            self.send_response(self.iopub_socket, 'stream',
+                               {'name': 'stdout', 'text': res[0].decode('utf-8')})
+
+            return {'status': 'ok', 'execution_count': self.execution_count, 'payload': [],
+                    'user_expressions': {}}
 
     def do_complete(self, code, cursor_pos):
         # TODO: Implement this
