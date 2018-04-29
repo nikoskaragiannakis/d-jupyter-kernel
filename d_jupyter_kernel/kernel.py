f007669045f3b1de93afd55da0f6a8884820ad17
Nikos Karagiannakis <nikoskaragiannakis@gmail.com>
2018-04-29 21:47:39 +0100

    better declaration handnling

--- d_jupyter_kernel/kernel.py
+++ d_jupyter_kernel/kernel.py
@@ -12,6 +12,13 @@ from ipykernel.kernelbase import Kernel
 __version__ = '1.0'
 
 
+FUNC_PATTERN = re.compile(r"(\w+)\s+(\w+)\s*\((.*?)\)\s*\{(.*?)\}", re.S)
+CLASS_PATTERN = re.compile(r"class (.*?)\s*\{(.*?)\}", re.S)
+STRUCT_PATTERN = re.compile(r"struct (\w+)\s*\{(.*?)\}", re.S)
+DECL_PATTERN = re.compile(r"^(\w+\s+)*(\w+)\s*\=\s*(.*)", re.MULTILINE)
+IMPORT_PATTERN = re.compile(r"import\s+([\w+.,:= ]+);", re.S)
+
+
 class CompilationError(Exception):
     pass
 
@@ -36,7 +43,7 @@ class DKernel(Kernel):
                      'file_extension': '.d'}
 
     def __init__(self, **kwargs):
-        self.buffer = {'imports': [], 'funcs': {}}
+        self.buffer = {'imports': {}, 'funcs': {}, 'classes': {}, 'structs': {}, 'decls': {}}
 
         self.func_def_pattern = re.compile(r"^\s*(\w+\s+)?\s*\w+\s+(\w+)\s*\([^)]*\)\s*\{.*?",
                                            re.MULTILINE)
@@ -61,13 +68,29 @@ class DKernel(Kernel):
                "executables in temporary folder.\n".format(self.language_version)
 
     def _output_code(self, code):
-        imports_str = '\n'.join(self.buffer['imports']) if self.buffer['imports'] else ''
-        funcs_str = '\n'.join(list(self.buffer['funcs'].values())) if self.buffer['funcs'] else ''
-        main_str = "void main() {{{}}}".format(code)
+        imports_str = ('\n'.join(list(self.buffer['imports'].values()))
+                       if self.buffer['imports'] else '')
+        funcs_str = ('\n'.join(list(self.buffer['funcs'].values()))
+                     if self.buffer['funcs'] else '')
+        classes_str = ('\n'.join(list(self.buffer['classes'].values()))
+                       if self.buffer['classes'] else '')
+        structs_str = ('\n'.join(list(self.buffer['structs'].values()))
+                       if self.buffer['structs'] else '')
+        decl_str = ('\n'.join(list(self.buffer['decls'].values()))
+                    if self.buffer['decls'] else '')
 
-        return '{}\n\n{}\n\n{}'.format(imports_str, funcs_str, main_str)
+        if any(re.match(pattern, code) for pattern in (IMPORT_PATTERN, FUNC_PATTERN,
+                                                       CLASS_PATTERN, STRUCT_PATTERN,
+                                                       DECL_PATTERN)):
+            main_code = ''
+        else:
+            main_code = code
+        main_str = "void main() {{{}}}".format(main_code)
 
-    def _execute_code(self, code, is_func=False, is_import=False):
+        return '{}\n\n{}\n\n{}\n\n{}\n\n{}\n\n{}'.format(imports_str, classes_str, structs_str,
+                                                         funcs_str, decl_str, main_str)
+
+    def _execute_code(self, code):
         with tempdir() as temp_dir:
             with tempfile.NamedTemporaryFile(prefix='d_main_',
                                              suffix='.d',
@@ -88,6 +111,7 @@ class DKernel(Kernel):
                     rows = stderr.decode('utf-8').split('\n')
                     rows = [''.join(row.replace(d_file.name, 'line')) for row in rows]
                     stderr = '\n'.join(rows)
+
                     self.send_response(self.iopub_socket, 'stream',
                                        {'name': 'stdout', 'text': stderr})
                     raise CompilationError(stderr)
@@ -109,23 +133,30 @@ class DKernel(Kernel):
         os.remove('{}.o'.format(base_name))
         os.remove(base_name)
 
+    def _remove_added(self, added):
+        for section in self.buffer:
+            self.buffer[section] = {k: v
+                                    for k, v in self.buffer[section].items()
+                                    if k not in added[section]}
+
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=True):
         self._allow_stdin = allow_stdin
+        added = {}
         if not silent:
-                try:
-                    self._execute_code(code)
-                except CompilationError:
-                    pass
-                else:
-                    if re.match(self.func_def_pattern, code):
-                        func_name = re.findall(self.func_def_pattern, code)[0][1]
-                        self.buffer['funcs'][func_name] = code
-                    elif re.match(self.import_pattern, code):
-                        self.buffer['imports'].append(code)
-                finally:
-                    return {'status': 'ok', 'execution_count': self.execution_count,
-                            'payload': [], 'user_expressions': {}}
+            added['classes'] = self._get_classes(code)
+            added['decls'] = self._get_decls(code)
+            added['funcs'] = self._get_funcs(code)
+            added['structs'] = self._get_structs(code)
+            added['imports'] = self._get_imports(code)
+
+            try:
+                self._execute_code(code)
+            except CompilationError:
+                self._remove_added(added)
+            finally:
+                return {'status': 'ok', 'execution_count': self.execution_count,
+                        'payload': [], 'user_expressions': {}}
 
     def do_complete(self, code, cursor_pos):
         # TODO: Implement this
@@ -134,3 +165,97 @@ class DKernel(Kernel):
     def do_inspect(self, code, cursor_pos, detail_level=0):
         # TODO: Implement this
         return super().do_inspect(code, cursor_pos, detail_level=detail_level)
+
+    def _get_classes(self, code):
+        classes = re.findall(CLASS_PATTERN, code)
+        added = set()
+        for class_ in classes:
+            name = class_[0].strip()
+            if ':' in name:
+                name, subclass = name.split(':')
+            else:
+                subclass = ''
+            body = '{{{}}}'.format(class_[1])
+            if subclass:
+                res = ('class {}:{} {}'.format(name, subclass, body))
+            else:
+                res = ('class {} {}'.format(name, body))
+
+            self.buffer['classes'][name] = res
+            added.add(name)
+        return added
+
+    def _get_funcs(self, code):
+        funcs = re.findall(FUNC_PATTERN, code)
+        added = set()
+        for func in funcs:
+            type_ = func[0].strip()
+            name = func[1].strip()
+            params = '({})'.format(func[2])
+            body = '{{{}}}'.format(func[3])
+            res = ('{} {} {} {}'.format(type_, name, params, body))
+
+            self.buffer['funcs'][name] = res
+            added.add(name)
+        return added
+
+    def _get_structs(self, code):
+        structs = re.findall(STRUCT_PATTERN, code)
+        added = set()
+        for struct in structs:
+            name = struct[0].strip()
+            body = '{{{}}}'.format(struct[1])
+            res = ('{} {}'.format(name, body))
+
+            self.buffer['structs'][name] = res
+            added.add(name)
+        return added
+
+    def _get_decls(self, code):
+        decls = re.findall(DECL_PATTERN, code)
+        added = set()
+        for decl in decls:
+            type_ = decl[0].strip()
+            name = decl[1].strip()
+            value = decl[2]
+            res = ('{}={}'.format(name, value))
+            if type_:
+                res = '{} {}'.format(type_, res)
+
+            self.buffer['decls'][name] = res
+            added.add(name)
+        return added
+
+    def _get_imports(self, code):
+        imports = re.findall(IMPORT_PATTERN, code)
+        added = set()
+        for imp in imports:
+            bases, *mods = imp.split(':')
+            if mods:
+                if '=' in base:
+                    base, base_alias = bases.split('=')
+                    base_str = '{} = {}'.format(base, base_alias)
+                else:
+                    base_str = base
+                for mod in mods:
+                    mod = mod.strip()
+                    if '=' in mod:
+                        mod, mod_alias = mod.split('=')
+                        mod_str = '{} = {}'.format(mod, mod.alias)
+                    else:
+                        mod_str = mod
+                    self.buffer['imports']['{}.{}'.format(base, mod)] = 'import {} : {};'.format(
+                        base_str, mod_str)
+                    added.add('{}.{}'.format(base, mod))
+            else:
+                for base in bases.split(','):
+                    base = base.strip()
+                    if '=' in base:
+                        base, base_alias = base.split('=')
+                        base_str = '{} = {}'.format(base, base_alias)
+                    else:
+                        base_str = base
+                    self.buffer['imports'][base] = 'import {};'.format(base_str)
+                    added.add(base)
+
+        return added
